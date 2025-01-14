9530d97f18176504ab25571b7ead7a48a2f56a20
sbakhareva <snezhana.bakhareva@yandex.ru>
2025-01-14 09:02:41 +0300

    начальный коммит

---
+++ src/test/java/org/skypro/skyshop_2_0/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.skyshop_2_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
