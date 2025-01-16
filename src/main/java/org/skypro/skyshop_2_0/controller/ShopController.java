c11dc1c53ab65c732a4a1fedcc65138b6d3fa8fa
sbakhareva <snezhana.bakhareva@yandex.ru>
2025-01-16 12:53:16 +0300

    на проверку

--- src/main/java/org/skypro/skyshop_2_0/controller/ShopController.java
+++ src/main/java/org/skypro/skyshop_2_0/controller/ShopController.java
@@ -2,21 +2,25 @@ package org.skypro.skyshop_2_0.controller;
 
 import org.skypro.skyshop_2_0.model.article.Article;
 import org.skypro.skyshop_2_0.model.product.Product;
+import org.skypro.skyshop_2_0.model.search.SearchResult;
+import org.skypro.skyshop_2_0.model.search.Searchable;
+import org.skypro.skyshop_2_0.service.SearchService;
 import org.skypro.skyshop_2_0.service.StorageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 public class ShopController {
     private final StorageService storageService;
+    private final SearchService searchService;
 
-    public ShopController(StorageService storageService) {
+    public ShopController(StorageService storageService, SearchService searchService) {
         this.storageService = storageService;
+        this.searchService = searchService;
     }
 
     @GetMapping("/products")
@@ -27,5 +31,10 @@ public class ShopController {
     @GetMapping("/articles")
     public Map<UUID, Article> getAllArticles() {
         return storageService.getArticles();
+    }
+
+    @GetMapping("/search")
+    public Set<Searchable> search(String pattern) {
+        return searchService.search(pattern);
     }
 }
--- src/main/java/org/skypro/skyshop_2_0/service/SearchService.java
+++ src/main/java/org/skypro/skyshop_2_0/service/SearchService.java
@@ -1,6 +1,12 @@
 package org.skypro.skyshop_2_0.service;
 
+import org.skypro.skyshop_2_0.model.search.SearchResult;
+import org.skypro.skyshop_2_0.model.search.Searchable;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class SearchService {
@@ -10,11 +16,12 @@ public class SearchService {
         this.storageService = storageService;
     }
 
-//    public Set<SearchResult> search(String search) {
-//        Set<SearchResult> searchResults = storageService.getSearchableCollection().stream()
-//                .filter(searchable -> searchable.getSearchTerm().toLowerCase().replace(" ", "")
-//                        .contains(search.toLowerCase().replace(" ", "")))
-//                .collect(Collectors.toCollection();
-//        return searchResults;
-//    }
+    public Set<Searchable> search(String search) {
+        Set<SearchResult> searchResults = new HashSet<>();
+        Set<Searchable> searchResults2 = storageService.getSearchableCollection().stream()
+                .filter(searchable -> searchable.getSearchTerm().toLowerCase().replace(" ", "")
+                        .contains(search.toLowerCase().replace(" ", "")))
+                .collect(Collectors.toCollection(HashSet::new));
+        return searchResults2;
+    }
 }
