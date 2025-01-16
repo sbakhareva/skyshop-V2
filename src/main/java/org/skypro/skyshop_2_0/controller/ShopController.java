b141c7f679a05e624ef7aca60422ef71a3e255ec
sbakhareva <snezhana.bakhareva@yandex.ru>
2025-01-16 22:49:32 +0300

    на проверку

--- src/main/java/org/skypro/skyshop_2_0/controller/ShopController.java
+++ src/main/java/org/skypro/skyshop_2_0/controller/ShopController.java
@@ -1,17 +1,12 @@
 package org.skypro.skyshop_2_0.controller;
-
 import org.skypro.skyshop_2_0.model.article.Article;
 import org.skypro.skyshop_2_0.model.product.Product;
 import org.skypro.skyshop_2_0.model.search.SearchResult;
-import org.skypro.skyshop_2_0.model.search.Searchable;
 import org.skypro.skyshop_2_0.service.SearchService;
 import org.skypro.skyshop_2_0.service.StorageService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestController
 public class ShopController {
--- src/main/java/org/skypro/skyshop_2_0/service/SearchService.java
+++ src/main/java/org/skypro/skyshop_2_0/service/SearchService.java
@@ -1,10 +1,6 @@
 package org.skypro.skyshop_2_0.service;
-
 import org.skypro.skyshop_2_0.model.search.SearchResult;
-import org.skypro.skyshop_2_0.model.search.Searchable;
 import org.springframework.stereotype.Service;
-
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -17,11 +13,10 @@ public class SearchService {
     }
 
     public Set<SearchResult> search(String search) {
-        Set<SearchResult> searchResults = storageService.getSearchableCollection().stream()
+        return storageService.getSearchableCollection().stream()
                 .filter(searchlist -> searchlist.getSearchTerm().toLowerCase().replace(" ", "")
                         .contains(search.toLowerCase().replace(" ", "")))
                 .map(SearchResult::fromSearchable)
-                .collect(Collectors.toCollection(HashSet::new));
-        return searchResults;
+                .collect(Collectors.toSet());
     }
 }
